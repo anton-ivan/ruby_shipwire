876b2e1335694795ac1d803bdd7cdea04f5e2938
Anton Ivanov <an.ivan0209@gmail.com>
2016-01-31 18:09:31 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128170719) do
+ActiveRecord::Schema.define(version: 20160131043548) do
+
+  create_table "agents", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "customer_cards", force: true do |t|
     t.integer  "customer_id"
@@ -171,6 +178,7 @@ ActiveRecord::Schema.define(version: 20160128170719) do
     t.datetime "updated_at"
     t.float    "weight"
     t.string   "sku"
+    t.string   "product_type"
   end
 
   create_table "recurrent_products", force: true do |t|
