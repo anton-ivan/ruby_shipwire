25e563dbdbad50e57e850911ee8b393bf8210efc
krishna <krishna.kumar@beyondapplications.com>
2016-02-02 02:19:41 +0530

    changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,12 @@
  
- every 10.minutes do
-   runner "DomainDistributor.get_orders"
- end 
+every 10.minutes do
+  #runner "DomainDistributor.get_orders"
+end  
+
+every :day, :at => '12:20am' do
+  rake "hair:generate_recurrent_orders"
+end
+
+every :day, :at => '12:50am' do
+  rake "hair:create_shipwire_orders"
+end
